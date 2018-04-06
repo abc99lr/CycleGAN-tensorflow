12f7bb5abb92ad788248b351f7563356cfb2123f
Rui Lan <abc99lr@gmail.com>
2018-04-06 13:52:12 -0500

    changing channel

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ tf.set_random_seed(19)
 from model import cyclegan
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--dataset_dir', dest='dataset_dir', default='/home/ruilan2/scratch/MRI_CC_data', \
+parser.add_argument('--dataset_dir', dest='dataset_dir', default='/home/ruilan2/scratch/MRI_CC_data/', \
                     help='path of the dataset')
 parser.add_argument('--epoch', dest='epoch', type=int, default=200, help='# of epoch')
 parser.add_argument('--epoch_step', dest='epoch_step', type=int, default=100, help='# of epoch to decay lr')
@@ -15,8 +15,8 @@ parser.add_argument('--load_size', dest='load_size', type=int, default=286, help
 parser.add_argument('--fine_size', dest='fine_size', type=int, default=256, help='then crop to this size')
 parser.add_argument('--ngf', dest='ngf', type=int, default=64, help='# of gen filters in first conv layer')
 parser.add_argument('--ndf', dest='ndf', type=int, default=64, help='# of discri filters in first conv layer')
-parser.add_argument('--input_nc', dest='input_nc', type=int, default=3, help='# of input image channels')
-parser.add_argument('--output_nc', dest='output_nc', type=int, default=3, help='# of output image channels')
+parser.add_argument('--input_nc', dest='input_nc', type=int, default=1, help='# of input image channels')
+parser.add_argument('--output_nc', dest='output_nc', type=int, default=1, help='# of output image channels')
 parser.add_argument('--lr', dest='lr', type=float, default=0.0002, help='initial learning rate for adam')
 parser.add_argument('--beta1', dest='beta1', type=float, default=0.5, help='momentum term of adam')
 parser.add_argument('--which_direction', dest='which_direction', default='AtoB', help='AtoB or BtoA')
@@ -24,7 +24,7 @@ parser.add_argument('--phase', dest='phase', default='train', help='train, test'
 parser.add_argument('--save_freq', dest='save_freq', type=int, default=1000, help='save a model every save_freq iterations')
 parser.add_argument('--print_freq', dest='print_freq', type=int, default=100, help='print the debug information every print_freq iterations')
 parser.add_argument('--continue_train', dest='continue_train', type=bool, default=False, help='if continue training, load the latest model: 1: true, 0: false')
-parser.add_argument('--checkpoint_dir', dest='checkpoint_dir', default='/home/ruilan2/scratch/MPR0_CCResult', \
+parser.add_argument('--checkpoint_dir', dest='checkpoint_dir', default='/home/ruilan2/scratch/MPR0_CCResult/', \
                     help='models are saved here')
 parser.add_argument('--sample_dir', dest='sample_dir', default='./sample', help='sample are saved here')
 parser.add_argument('--test_dir', dest='test_dir', default='./test', help='test sample are saved here')
