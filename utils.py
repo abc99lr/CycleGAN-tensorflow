f4a975a3e9ee00eeb9b6944cd0db6e8dfc7fc362
Rui Lan <abc99lr@gmail.com>
2018-04-07 21:53:46 -0500

    debug img size

--- utils.py
+++ utils.py
@@ -54,8 +54,8 @@ def load_train_data(image_path, load_size=286, fine_size=256, is_testing=False):
     load_H = 256
     load_W = 320
     if not is_testing:
-        img_A = scipy.misc.imresize(img_A, [load_H, load_W, 1])
-        img_B = scipy.misc.imresize(img_B, [load_H, load_W, 1])
+        img_A = scipy.misc.imresize(img_A, [load_H, load_W])
+        img_B = scipy.misc.imresize(img_B, [load_H, load_W])
         h1 = int(np.ceil(np.random.uniform(1e-2, load_H-fine_size)))
         w1 = int(np.ceil(np.random.uniform(1e-2, load_W-fine_size)))
         img_A = img_A[h1:h1+fine_size, w1:w1+fine_size]
@@ -70,11 +70,15 @@ def load_train_data(image_path, load_size=286, fine_size=256, is_testing=False):
 
     img_A = img_A/127.5 - 1.
     img_B = img_B/127.5 - 1.
-    
+    img_A_3 = np.zeros([fine_size, fine_size, 1])
+    img_B_3 = np.zeros([fine_size, fine_size, 1])
+
+    img_A_3[:, :, 0] = img_A
+    img_B_3[:, :, 0] = img_B
     # img_A = np.reshape(img_A, [fine_size, fine_size, 1])
     # img_B = np.reshape(img_B, [fine_size, fine_size, 1])
 
-    img_AB = np.concatenate((img_A, img_B), axis=2)
+    img_AB = np.concatenate((img_A_3, img_B_3), axis=2)
     # img_AB shape: (fine_size, fine_size, input_c_dim + output_c_dim)
     return img_AB
 
