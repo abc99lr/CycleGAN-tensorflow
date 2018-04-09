103143659c33368faaf158fe10b182325c718624
Rui Lan <abc99lr@gmail.com>
2018-04-08 19:54:48 -0500

    debug resize

--- utils.py
+++ utils.py
@@ -51,26 +51,28 @@ def load_test_data(image_path, fine_size=256):
 def load_train_data(image_path, load_size=286, fine_size=256, is_testing=False):
     img_A = imread(image_path[0])
     img_B = imread(image_path[1])
-    load_H = img_A.shape[0]
-    load_W = img_A.shape[1]
+    # load_H = img_A.shape[0]
+    # load_W = img_A.shape[1]
 
-    print(load_H)
-    print(load_W)
-    
-    if not is_testing:
-        img_A = scipy.misc.imresize(img_A, [load_H, load_W])
-        img_B = scipy.misc.imresize(img_B, [load_H, load_W])
-        # h1 = int(np.ceil(np.random.uniform(1e-2, load_H-fine_size)))
-        # w1 = int(np.ceil(np.random.uniform(1e-2, load_W-fine_size)))
-        # img_A = img_A[h1:h1+fine_size, w1:w1+fine_size]
-        # img_B = img_B[h1:h1+fine_size, w1:w1+fine_size]
+    # print(load_H)
+    # print(load_W)
 
-        # if np.random.random() > 0.5:
-        #     img_A = np.fliplr(img_A)
-        #     img_B = np.fliplr(img_B)
-    else:
-        img_A = scipy.misc.imresize(img_A, [fine_size, fine_size])
-        img_B = scipy.misc.imresize(img_B, [fine_size, fine_size])
+    img_A = scipy.misc.imresize(img_A, [fine_size, fine_size])
+    img_B = scipy.misc.imresize(img_B, [fine_size, fine_size])
+    # if not is_testing:
+    #     img_A = scipy.misc.imresize(img_A, [load_H, load_W])
+    #     img_B = scipy.misc.imresize(img_B, [load_H, load_W])
+    #     # h1 = int(np.ceil(np.random.uniform(1e-2, load_H-fine_size)))
+    #     # w1 = int(np.ceil(np.random.uniform(1e-2, load_W-fine_size)))
+    #     # img_A = img_A[h1:h1+fine_size, w1:w1+fine_size]
+    #     # img_B = img_B[h1:h1+fine_size, w1:w1+fine_size]
+
+    #     # if np.random.random() > 0.5:
+    #     #     img_A = np.fliplr(img_A)
+    #     #     img_B = np.fliplr(img_B)
+    # else:
+    #     img_A = scipy.misc.imresize(img_A, [fine_size, fine_size])
+    #     img_B = scipy.misc.imresize(img_B, [fine_size, fine_size])
 
     img_A = img_A/127.5 - 1.
     img_B = img_B/127.5 - 1.
