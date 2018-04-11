558c770fc5083fd55c449270d4757041630f0fea
Lan <ruilan2@golubh1.campuscluster.illinois.edu>
2018-04-11 11:39:12 -0500

    run 4 epoch

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --dataset_dir /home/ruilan2/scratch/MRI_CC_data/ \
-    --epoch 8 \
+    --epoch 4 \
     --sample_dir /home/ruilan2/scratch/MPR0_CCSample/sample_0411/ \
     --save_freq 1000 \
     --print_freq 100 \
