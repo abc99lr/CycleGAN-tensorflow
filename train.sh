429c7638d8988364a9edc4bb35de294e6a7e5549
Rui Lan <abc99lr@gmail.com>
2018-04-11 15:31:24 -0500

    fix sample directory path

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --dataset_dir /home/ruilan2/scratch/MRI_CC_data/ \
     --epoch 4 \
-    --sample_dir /home/ruilan2/scratch/MPR0_CCSample/sample_0411/ \
+    --sample_dir /home/ruilan2/scratch/MPR0_CCSample/sample_0411 \
     --save_freq 1000 \
     --print_freq 100 \
     --continue_train True \
