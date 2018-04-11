aac6a8411273739b2fb513152a0c4a6ac48bd2d7
Rui Lan <abc99lr@gmail.com>
2018-04-11 14:51:06 -0500

    quick fix

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
 	--phase test \
 	--which_direction AtoB \
 	--test_dir /home/ruilan2/scratch/MPR0_CCTest/Test0411 \
-    --dataset_dir /home/ruilan2/scratch/MRI_CC_test_data/ \
+    --dataset_dir /home/ruilan2/scratch/MRI_CC_test_data \
     --epoch 4 \
     --sample_dir /home/ruilan2/scratch/MPR0_CCSample/sample_0411/ \
     --save_freq 1000 \
