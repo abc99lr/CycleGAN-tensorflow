65d1e8c283375c3aae33816a9d7fb264a18c6f60
Rui Lan <abc99lr@gmail.com>
2018-04-13 14:41:56 -0500

    change resnet path

--- train.sh
+++ train.sh
@@ -2,8 +2,8 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --dataset_dir /home/ruilan2/scratch/MRI_CC_data/ \
     --epoch 10 \
-    --sample_dir /home/ruilan2/scratch/MPR0_CCSample/sample_0413-unet \
+    --sample_dir /home/ruilan2/scratch/MPR0_CCSample/sample_0413-resnet \
     --save_freq 1000 \
     --print_freq 100 \
     --continue_train 0 \
-    --checkpoint_dir /home/ruilan2/scratch/MPR0_CCResult/Model0413-unet
+    --checkpoint_dir /home/ruilan2/scratch/MPR0_CCResult/Model0413-resnet
