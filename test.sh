09422ccc98e40fb003b84c2883ece7130ce1ee4b
Rui Lan <abc99lr@gmail.com>
2018-04-16 15:37:27 -0500

    test on unet

--- test.sh
+++ test.sh
@@ -4,5 +4,6 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
 	--which_direction AtoB \
 	--test_dir /home/ruilan2/scratch/MPR0_CCTest/Test0416-unet \
     --dataset_dir /home/ruilan2/scratch/MRI_CC_test_data \
+    --use_resnet False \
     --continue_train True \
     --checkpoint_dir /home/ruilan2/scratch/MPR0_CCResult/Model0413-unet/cyclegan.model-35001
