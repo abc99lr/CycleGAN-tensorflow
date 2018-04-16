829ca34d5f10358567156bc10aac4fb6a909e6b7
Rui Lan <abc99lr@gmail.com>
2018-04-16 15:25:43 -0500

    adding resnet test

--- test.sh
+++ test.sh
@@ -2,11 +2,7 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
 	--phase test \
 	--which_direction AtoB \
-	--test_dir /home/ruilan2/scratch/MPR0_CCTest/Test0413-2 \
+	--test_dir /home/ruilan2/scratch/MPR0_CCTest/Test0416-resnet \
     --dataset_dir /home/ruilan2/scratch/MRI_CC_test_data \
-    --epoch 4 \
-    --sample_dir /home/ruilan2/scratch/MPR0_CCSample/sample_0413/ \
-    --save_freq 1000 \
-    --print_freq 100 \
     --continue_train True \
-    --checkpoint_dir /home/ruilan2/scratch/MPR0_CCResult/cyclegan.model-14001
+    --checkpoint_dir /home/ruilan2/scratch/MPR0_CCResult/Model0413-resnet/cyclegan.model-35001
